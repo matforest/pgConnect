ae5eb58c52ffe0026305c1cbc22e227f30a1fb1f
matf <mat@forest.id.au>
2013-05-30 21:56:58 +0930

    Basic query working

--- connector.js
+++ connector.js
@@ -1,5 +1,5 @@
 var pg = require('pg');
-var conString = "postgres://munt:pass@192.168.1.1:5432/geodata";
+var conString = "postgres://munt:pass@localhost:5432/geodata";
 
 var client = new pg.Client(conString);
 console.log('connecting');
@@ -9,14 +9,13 @@ console.log('connected');
 
 function afterConnect(err) {
 
-	console.log('afterConnect err: ' + err);
+	console.log('afterConnect err: ', err);
 	if (err) {
 		throw err;
 	}
 
-	//var query = client.query("SELECT * FROM testing.temp;");
 	console.log('querying');
-	var query = client.query("fart fart fart;");
+	var query = client.query("SELECT * FROM testing.temp;");
 	console.log('query done');
 
 	query.on('row', function(row) {
